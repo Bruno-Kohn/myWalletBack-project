768c8e3ac0b966107eb99ef6189718031e0d48c1
Bruno Kohn <brunokohn1993@gmail.com>
2021-10-22 00:08:54 -0300

    login and sign-up routes done

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ import cors from "cors";
 import pg from "pg";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
+import Joi from "joi";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 //-------------------- Connection --------------------
 
@@ -13,7 +15,7 @@ const { Pool } = pg;
 
 const connection = new Pool({
   user: "postgres",
-  password: "password",
+  password: "123456",
   host: "localhost",
   port: 5432,
   database: "mywallet",
@@ -22,15 +24,23 @@ const connection = new Pool({
 //-------------------- Register / Sign-up --------------------
 
 app.post("/register", async (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(5).required(),
+    passwordConfirmation: Joi.string().min(5).required(),
+  });
+
+  const value = schema.validate(req.body);
+  console.log(value);
   const { name, email, password, passwordConfirmation } = req.body;
 
   if (password !== passwordConfirmation) {
     return res.sendStatus(400);
   }
 
-  const passwordHash = bcrypt.hashSync(password, 11);
-
   try {
+    const passwordHash = bcrypt.hashSync(password, 11);
     await connection.query(
       `INSERT INTO users
         (name, email, password) 
@@ -39,6 +49,7 @@ app.post("/register", async (req, res) => {
     );
     res.sendStatus(201);
   } catch (error) {
+    res.sendStatus(500);
     console.log(error);
   }
 });
@@ -46,9 +57,16 @@ app.post("/register", async (req, res) => {
 //-------------------- Login / Sign-in --------------------
 
 app.post("/", async (req, res) => {
-  const { email, password } = req.body;
+  const schema = Joi.object({
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .required(),
+    password: Joi.string().min(5).required(),
+  });
 
   try {
+    const value = await schema.validateAsync(req.body);
+    const { email, password } = value;
     const result = await connection.query(
       `SELECT * FROM users
         WHERE email = $1`,
@@ -58,18 +76,45 @@ app.post("/", async (req, res) => {
     const user = result.rows[0];
 
     if (user && bcrypt.compareSync(password, user.password)) {
-      const token = uuid.v4();
+      const token = uuid();
 
       await connection.query(
-        `INSERT INTO session 
+        `INSERT INTO sessions 
           ("userId", token) 
           VALUES ($1, $2)`,
         [user.id, token]
       );
-      res.send(token).status(201);
+      res.send({token, name: user.name}).status(201);
     } else {
       return res.sendStatus(401);
     }
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+});
+
+//-------------------- Post Records --------------------
+
+app.get("/records", async (req, res) => {
+  const authorization = req.headers.authorization;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) return res.sendStatus(401);
+
+  try {
+    const result = await connection.query(
+      `
+    SELECT * FROM sessions
+    JOIN users
+    ON sessions."userId" = users.id
+    WHERE sessions.token = $1
+      `,
+      [token]
+    );
+
+    const userToken = result.rows[0];
+    res.send(userToken).status(201);
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
